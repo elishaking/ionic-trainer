93332768cf43593e60dccb600d72ed5bd8b29ecc
elishaking <ek.chibueze@gmail.com>
2018-03-08 22:53:23 +0100

    activities del

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,6 +14,9 @@ export class HomePage {
   activities: Activity[] = [];
   userRef: Reference;
 
+  checked: boolean[] = [];
+  showDeleteBtn = false;
+
   constructor(public navCtrl: NavController, private navParams: NavParams, private storage: Storage) {
     this.userRef = this.navParams.get('userRef') || this.userRef;
   }
@@ -22,11 +25,33 @@ export class HomePage {
     console.log(this.userRef);
     this.storage.get('activities').then((activities) => {
       this.activities = activities ? activities : [];
+      this.updateChecked();
     });
+  }
+
+  updateChecked(){
+    for(let i = 0; i < this.activities.length; i++){
+      this.checked.push(false);
+    }
   }
 
   contact(){
     this.navCtrl.push(ContactUsPage);
+  }
+
+  toggleDeleteFab(){
+    this.showDeleteBtn = this.checked.indexOf(true) != -1;
+  }
+
+  delete(){
+    for(let i = 0; i < this.activities.length; i++){
+      if(this.checked[i]){
+        this.checked.splice(i, 1);
+        this.activities.splice(i, 1);
+        i--;
+      }
+    }
+    this.showDeleteBtn = false;
   }
 
 }
