f47b40318d63a1f19a41a9d1a40dcbf6e6420ccf
elishaking <ek.chibueze@gmail.com>
2018-03-08 01:12:34 +0100

    identity

--- src/pages/create-identity/create-identity.ts
+++ src/pages/create-identity/create-identity.ts
@@ -3,6 +3,8 @@ import { NavController, NavParams, ToastController } from 'ionic-angular';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { Storage } from '@ionic/storage';
+import { Identity, Activity } from '../../models/interfaces';
+import { getDayTime } from '../../models/functions';
 
 @Component({
   selector: 'page-create-identity',
@@ -24,6 +26,12 @@ export class CreateIdentityPage {
 
   ionViewDidLoad() {
     // console.log('ionViewDidLoad CreateIdentityPage');
+    this.storage.get('identity').then((identity: Identity) => {
+      if(identity){
+        this.createIdentityForm.controls.athleticDreams.setValue(identity.athleticDreams);
+        this.createIdentityForm.controls.attitudeSlogan.setValue(identity.attitudeSlogan);
+      }
+    });
   }
 
   save(){
@@ -37,6 +45,15 @@ export class CreateIdentityPage {
         attitudeSlogan: this.createIdentityForm.value['attitudeSlogan']
       }).then(() => {
         this.presentToast('Identity Saved');
+        let date = getDayTime();
+        this.storage.get('activities').then((activities: Activity[]) => {
+          let a = activities ? activities : [];
+          a.push({
+            title: 'Updated Identity',
+            date: date[0] + " " + date[1]
+          });
+          this.storage.set('activities', a);
+        });
       }, () => {
         this.presentToast('Error occurred while saving identity');
       });
