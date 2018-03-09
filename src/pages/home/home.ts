dc9a4abb48f5a59ca441a8dedc8ec8b424f23b18
elishaking <ek.chibueze@gmail.com>
2018-03-09 16:55:32 +0100

    home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Activity } from '../../models/interfaces';
+import { Activity, Identity, Faith } from '../../models/interfaces';
 import { Storage } from '@ionic/storage';
 // import { ThenableReference } from '@firebase/database-types';
 import { Reference } from '@firebase/database';
 import { ContactUsPage } from '../contact-us/contact-us';
+import { EmailComposer } from '@ionic-native/email-composer';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  identity: Identity;
+  faith: Faith;
+
   activities: Activity[] = [];
   userRef: Reference;
 
   checked: boolean[] = [];
   showDeleteBtn = false;
 
-  constructor(public navCtrl: NavController, private navParams: NavParams, private storage: Storage) {
+  constructor(public navCtrl: NavController, private navParams: NavParams, private storage: Storage,
+    private emailComposer: EmailComposer) {
     this.userRef = this.navParams.get('userRef') || this.userRef;
+
+    this.storage.get('identity').then((identity) => {
+      this.identity = identity;
+    });
+    this.storage.get('faith').then((faith) => {
+      this.faith = faith;
+    });
   }
 
   ionViewDidLoad(){
@@ -36,7 +48,22 @@ export class HomePage {
   }
 
   contact(){
-    this.navCtrl.push(ContactUsPage);
+    // this.navCtrl.push(ContactUsPage);
+    let email = {
+      to: 'darreljordan1980@gmail.com',
+      // bcc: ['john@doe.com', 'jane@doe.com'],
+      // attachments: [
+      //   'file://img/logo.png',
+      //   'res://icon.png',
+      //   'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+      //   'file://README.pdf'
+      // ],
+      // subject: 'BdTough: Message from ' + msg.name + ': ' + msg.email,
+      // body: '',
+      // isHtml: true
+    };
+    
+    this.emailComposer.open(email);
   }
 
   toggleDeleteFab(){
